f3fd0b7ed28696187b170765f48e41db6da500e6
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-13 09:48:37 +0100

    first commit

--- Source/AosoFront/src/app/app.component.ts
+++ Source/AosoFront/src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
 
-    this.http.get<any[]>('http://localhost:4001/WeatherForecast' , httpOptions).subscribe(
+    this.http.get<any[]>('http://localhost:4003/WeatherForecast' , httpOptions).subscribe(
       res => {
         this.weatherList = res;
         console.log('helooo',res);
