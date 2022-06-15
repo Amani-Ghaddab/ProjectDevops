cb02556e4ced224ddc8c5f48f48f4075c810ee59
Amani Ghaddab <ameni.ghaddabe@gmail.com>
2022-06-15 20:47:20 +0100

    first commit

--- Source/AosoBack/Controllers/WeatherForecastController.cs
+++ Source/AosoBack/Controllers/WeatherForecastController.cs
@@ -5,14 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Threading.Tasks;
-namespace DevOpsProject.Controllers
+
 {
     [ApiController]
     [Route("[controller]")]
